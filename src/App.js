dd55e8596b1178f6f29bc322f98fa96c26f17bb2
nauman <nsayed1@asu.edu>
2023-12-14 23:17:47 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import './App.css';
+// import './Components/footer/footer'
+import Footer from '../src/Components/Footer/Footer'
+
+function App() {
+  return (
+    // <div>
+    // <div className="bar">
+    //   <span className='h'>Home</span>
+    //   <span className='p'>Product</span>
+    //   <span className='company'>Company</span>
+    //   <span className='c'>Contact</span>
+    //   </div>
+    //   <div className='middle'><p className='part'>PART - 1</p></div>
+    //   <div className='info'>
+    //     <div>
+    //       <h1>About</h1>
+    //       <p className='about'>To bear, the might, and makes of dels we have his resolence of us coil, must give under them? To die: than fly to sling ents the in thus patient a sleep; to sleep.</p>
+    //     </div>
+    //     <div>
+    //       <h2>Company</h2>
+    //       <p className='company'>To bear, the might, and makes of dels we have his resolence of us coil, must give under them? To die: than fly to sling ents the in thus patient a sleep; to sleep.</p>
+    //     </div>
+    //     <div>
+    //       <h3>Services</h3>
+    //       <p className='services'>To bear, the might, and makes of dels we have his resolence of us coil, must give under them? To die: than fly to sling ents the in thus patient a sleep; to sleep.</p>
+    //     </div>
+    //   </div>
+    //   </div>
+    <div className='main-container'>
+       <Footer/>
+    </div>
+   
+  );
+}
+
+export default App;
---
+++ src/Components/Footer/Footer.js
@@ -0,0 +1,14 @@
+import './Footer.css';
+
+function Footer() {
+  return (
+<div className='footer-container'>
+  <div className='box-1'></div>
+  <div className='box-2'></div>
+  <div className='box-3'></div>
+  <div className='box-4'></div>
+</div>
+  );
+}
+
+export default Footer;
