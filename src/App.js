7986406759d5461c6310d88bce52486b76ec442e
nauman <nsayed1@asu.edu>
2023-12-14 23:26:13 -0500

    add nav

--- src/App.js
+++ src/App.js
@@ -1,36 +1,32 @@
 import './App.css';
-// import './Components/footer/footer'
+import Nav from '../src/Components/Nav/Nav'
 import Footer from '../src/Components/Footer/Footer'
 
 function App() {
   return (
-    // <div>
-    // <div className="bar">
-    //   <span className='h'>Home</span>
-    //   <span className='p'>Product</span>
-    //   <span className='company'>Company</span>
-    //   <span className='c'>Contact</span>
-    //   </div>
-    //   <div className='middle'><p className='part'>PART - 1</p></div>
-    //   <div className='info'>
-    //     <div>
-    //       <h1>About</h1>
-    //       <p className='about'>To bear, the might, and makes of dels we have his resolence of us coil, must give under them? To die: than fly to sling ents the in thus patient a sleep; to sleep.</p>
-    //     </div>
-    //     <div>
-    //       <h2>Company</h2>
-    //       <p className='company'>To bear, the might, and makes of dels we have his resolence of us coil, must give under them? To die: than fly to sling ents the in thus patient a sleep; to sleep.</p>
-    //     </div>
-    //     <div>
-    //       <h3>Services</h3>
-    //       <p className='services'>To bear, the might, and makes of dels we have his resolence of us coil, must give under them? To die: than fly to sling ents the in thus patient a sleep; to sleep.</p>
-    //     </div>
-    //   </div>
-    //   </div>
-    <div className='main-container'>
+    <>
+    <div>
+    <Nav/>
+      <div className='middle'><p className='part'>PART - 1</p></div>
+      <div className='info'>
+        <div>
+          <h1>About</h1>
+          <p className='about'>To bear, the might, and makes of dels we have his resolence of us coil, must give under them? To die: than fly to sling ents the in thus patient a sleep; to sleep.</p>
+        </div>
+        <div>
+          <h2>Company</h2>
+          <p className='company'>To bear, the might, and makes of dels we have his resolence of us coil, must give under them? To die: than fly to sling ents the in thus patient a sleep; to sleep.</p>
+        </div>
+        <div>
+          <h3>Services</h3>
+          <p className='services'>To bear, the might, and makes of dels we have his resolence of us coil, must give under them? To die: than fly to sling ents the in thus patient a sleep; to sleep.</p>
+        </div>
+      </div>
+      </div>
+    <div className=''>
        <Footer/>
     </div>
-   
+    </>
   );
 }
 
---
+++ src/Components/Nav/Nav.js
@@ -0,0 +1,14 @@
+import './Nav.css';
+
+function Nav() {
+  return (
+<div className="bar">
+<span className='h'>Home</span>
+<span className='p'>Product</span>
+<span className='company'>Company</span>
+<span className='c'>Contact</span>
+</div>
+  );
+}
+
+export default Nav;
